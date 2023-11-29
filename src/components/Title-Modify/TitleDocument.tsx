8739549c97a6e3348e95763235b1afaf31226f92
unknown <am247249@gmail.com>
2023-11-29 13:37:34 +0200

    Delete all files

--- src/components/Title-Modify/TitleDocument.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export default function TitleDocument(text: string) {
-  // useEffect
-  useEffect(() => {
-    document.title = text;
-  }, []);
-}
--- src/pages/Quran/PopUp.tsx
+++
@@ -1,7 +0,0 @@
-export default function PopUp() {
-  return (
-    <div className="w-fit bg-green-header text-white fixed py-3 px-8 right-1/2 z-20 text-3xl animate-opacity rounded-xl font-cairo">
-      <p>تم الحفظ!</p>
-    </div>
-  );
-}
