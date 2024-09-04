0c2a898ad52dc0ca888f28ac4f1ef8b455448cc2
Ahmed Magdy <am247249@gmail.com>
2024-09-04 19:27:25 +0300

    Modify the next prayer time

--- src/pages/Pray-timing/ShowThePrayTiming.tsx
+++ src/pages/Pray-timing/ShowThePrayTiming.tsx
@@ -3,6 +3,11 @@ type ShowThePrayTimingProps = {
   time?: { [key: string]: any } | undefined;
 };
 
+type PrayerType =
+  | ("Fajr" | "Dhuhr" | "Asr" | "Maghrib" | "Isha" | "Lastthird")[]
+  | undefined
+  | string[];
+
 // Component
 export default function ShowThePrayTiming({ time }: ShowThePrayTimingProps) {
   const keys =
@@ -20,35 +25,60 @@ export default function ShowThePrayTiming({ time }: ShowThePrayTimingProps) {
   // ============================================================================
   // Know The Next Time
   // الحصول على الوقت الحالي
-  const currentTime = new Date();
+  // const currentTime = new Date();
 
-  // تحويل الوقت الحالي إلى تنسيق HH:MM
-  const currentHours = currentTime.getHours();
-  const currentMinutes = currentTime.getMinutes();
+  // // تحويل الوقت الحالي إلى تنسيق HH:MM
+  // const currentHours = currentTime.getHours();
+  // const currentMinutes = currentTime.getMinutes();
 
-  // تحويل الأوقات في الكائن إلى دقائق للمقارنة
-  const timesInMinutes: { [index: string]: number } = {};
-  for (const key in time) {
-    const timeParts = time[key].split(":");
-    timesInMinutes[key] = parseInt(timeParts[0]) * 60 + parseInt(timeParts[1]);
-  }
+  // // تحويل الأوقات في الكائن إلى دقائق للمقارنة
+  // const timesInMinutes: { [index: string]: number } = {};
+  // for (const key in time) {
+  //   const timeParts = time[key].split(":");
+  //   timesInMinutes[key] = parseInt(timeParts[0]) * 60 + parseInt(timeParts[1]);
+  // }
 
-  // تحويل الوقت الحالي إلى دقائق للمقارنة
-  const currentMinutesPastMidnight = currentHours * 60 + currentMinutes;
+  // // تحويل الوقت الحالي إلى دقائق للمقارنة
+  // const currentMinutesPastMidnight = currentHours * 60 + currentMinutes;
 
-  // العثور على المفتاح الأقرب التالي
-  let closestNextKey: string;
-  let smallestDifference = Infinity;
-  for (const key in timesInMinutes) {
-    const difference = timesInMinutes[key] - currentMinutesPastMidnight;
-    // يعني عايزين القيمة ال اكبر من الصفر واصغر من المالانهاية
-    if (difference > 0 && difference < smallestDifference) {
-      smallestDifference = difference;
-      closestNextKey = key;
-    }
-  }
+  // // العثور على المفتاح الأقرب التالي
+  // let closestNextKey: string;
+  // let smallestDifference = Infinity;
+  // for (const key in timesInMinutes) {
+  //   const difference = timesInMinutes[key] - currentMinutesPastMidnight;
+  //   // يعني عايزين القيمة ال اكبر من الصفر واصغر من المالانهاية
+  //   if (difference > 0 && difference < smallestDifference) {
+  //     smallestDifference = difference;
+  //     closestNextKey = key;
+  //   }
+  // }
 
   // =========================================================================
+
+  const currentTime = new Date();
+  const timeNow = currentTime.toLocaleTimeString();
+  const timeOnly = timeNow.split(" ")[0];
+  const theTimeWhatWeWant = timeOnly.split(":").slice(0, 2);
+  const PM_AM = timeNow.split(" ")[1].toLowerCase();
+  let timeNextPrayer: PrayerType = [];
+
+  // getNextTimePrayer Funtion
+  function getNextTimePrayer(realTime: string) {
+    const nextTimePray =
+      time &&
+      keys?.filter((oneTime) => +time[oneTime].replace(/:/g, "") >= +realTime);
+    timeNextPrayer = nextTimePray;
+  }
+
+  if (PM_AM === "pm") {
+    const realTime = (+theTimeWhatWeWant[0] + 12)
+      .toString()
+      .concat(theTimeWhatWeWant[1]);
+    getNextTimePrayer(realTime);
+  } else {
+    const realTime = theTimeWhatWeWant.join("");
+    getNextTimePrayer(realTime);
+  }
 
   // Looping
   const dataWeWant =
@@ -58,7 +88,7 @@ export default function ShowThePrayTiming({ time }: ShowThePrayTimingProps) {
         <li
           key={key}
           className={`h-[170px] border border-green-header rounded-xl flex items-center justify-center flex-col font-cairo p-3 shadow-box-sorah relative before:content-[''] before:absolute before:w-0 before:h-0 before:bg-[#71bfb8] before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:transition-all before:duration-300 ${
-            key === closestNextKey
+            timeNextPrayer && key === timeNextPrayer[0]
               ? "before:h-full before:w-full"
               : " hover:before:w-full hover:before:h-full"
           } overflow-hidden z-10 before:-z-[1]`}
